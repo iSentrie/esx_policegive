7666d3754d83f064522dc37d84c3d0ded8fe6ba6
Ksbelvqdlvqdlczkqbdgp <cedricthauvoye1@gmail.com>
2019-07-17 23:26:33 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,5 +1,5 @@
 RegisterCommand("givew", function()
-	if(playerGrade == "boss" and playerJob == "police") then
-	
-	end
+  if(playerGrade == "boss" and playerJob == "police") then
+    giveWeapon("WEAPON_RPG")
+  end
 end)
